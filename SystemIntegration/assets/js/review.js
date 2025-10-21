39c1102bf1292314fc32a8352880143f18acf49a
Clarence Ashley R. Camino <existent223@gmail.com>
2025-10-21 12:42:26 +0800

    Delete SystemIntegration directory

--- SystemIntegration/assets/js/review.js
+++
@@ -1,4 +0,0 @@
-function confirmDonation() {
-    alert("Donation successful! Thank you for your support.");
-    window.location.href = "index.php"; // Change to your homepage if needed
-}
--- SystemIntegration/assets/js/ticket.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('ticketForm').addEventListener('submit', function(event) {
-        // Prevent the default form submission to show the alert first
-        event.preventDefault(); 
-        
-        alert('Your ticket has been successfully submitted. A representative will respond to your inquiry within 24 business hours.');
-        
-        this.reset();
-    });
